e0c820e0251c8a40c7670717f37fdef2b0d5a008
NataliIvantsova <ivancovanm@yandex.ru>
2023-01-27 22:32:53 +0300

    Primer 02&03

---
+++ Primer_02/Program.cs
@@ -0,0 +1,17 @@
+﻿//   Пишем через цикл FOR
+
+string Method4(int count, string c) // возвращать будем строку тип данных !!char!! Считывает один символ, он может быть пробел или символ конца строки.
+{
+    // будем строку "С" компоновать друг за дружкой компоновать count раз
+     //        string result = "" -  лежит пустая строка или написать String.Empty
+    string result = String.Empty;
+    for (int i = 0; i < count; i++)
+    {
+        result = result + c;
+    }
+    return result; // указываем ту переменную которую мы хотим получить из метода
+}
+
+/// вызов метода 4
+string res = Method4(10, "Cdtn");
+System.Console.WriteLine(res);
